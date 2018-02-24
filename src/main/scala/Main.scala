374bf3a2cd37466d3ecc397093c2d37c7b79a70b
robertbutacu <robert.butacu@yahoo.com>
2018-02-24 23:23:24 +0200

    PrintableSyntax

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,11 @@
 import printable.library.{Cat, Printable}
 import printable.library.PrintableInstances._
+import printable.library.PrintableSyntax._
 
 object Main extends App {
   val sophie = Cat("Sophie", 15, "red")
+
+  sophie.print
 
   Printable.print(sophie)
 }
