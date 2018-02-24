af33baa004b12f3e3d32455745f73b3e56773fff
robertbutacu <robert.butacu@yahoo.com>
2018-02-24 23:15:37 +0200

    fixed PrintableInstances for cats

--- src/main/scala/printable/library/PrintableInstances.scala
+++ src/main/scala/printable/library/PrintableInstances.scala
@@ -5,6 +5,13 @@ object PrintableInstances {
 
   implicit val printableInt: Printable[Int] = (value: Int) => value.toString
 
-  implicit val printableCat: Printable[Cat] = (value: Cat) =>
-    s"""${value.name} is a ${value.age} year-old ${value.color} cat."""
+  implicit val printableCat: Printable[Cat] = { (value: Cat) =>
+    lazy val formattedName = Printable.format(value.name)
+
+    lazy val formattedAge = Printable.format(value.age)
+
+    lazy val formattedColor = Printable.format(value.color)
+
+    s"""$formattedName is a $formattedAge year-old $formattedColor cat."""
+  }
 }
