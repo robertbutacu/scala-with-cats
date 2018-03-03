ecfe523b309b0d9cda5566fb3892676a0f62c1bc
robertbutacu <robert.butacu@yahoo.com>
2018-03-03 18:44:59 +0200

    example with option

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,13 @@ import printable.library.Cat
 import printable.library.Cat.catShow
 import cats.instances.list._
 import cats.instances.int._
+import cats.instances.option._
 import cats.syntax.semigroup._
+import cats.syntax.option._
 
 object Main extends App {
 
   def add[A: Monoid](l: List[A]): A = l.foldRight(Monoid[A].empty)((curr, acc) => curr |+| acc)
 
-  println(add(List(1,2,3,4,5)))
+  println(add[Option[Int]](List(1.some,2.some,3.some,4.some,5.some, none)))
 }
