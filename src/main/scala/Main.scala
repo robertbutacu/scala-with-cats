b37e1a66598101e646448921d7cfeaf8cc2d2ea0
robertbutacu <robert.butacu@yahoo.com>
2018-03-03 18:07:02 +0200

    Monoids/Semigroups for Boolean/Set[A]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,9 @@
 import printable.library.Cat
 import printable.library.Cat.catShow
 import cats.syntax.eq._
+import monoids.semigroups.Monoid
+import monoids.semigroups.Monoid.implicitAndBoolean
+import monoids.semigroups.Monoid.implicitSetMonoid
 
 object Main extends App {
   val sophie: Cat = Cat("Sophie", 15, "red")
@@ -8,4 +11,8 @@ object Main extends App {
 
   println(sophie === eva)
   println(sophie === sophie)
+
+  println(Monoid[Boolean].combine(true, false))
+
+  println(Monoid[Set[Int]].combine(Set(1, 2, 3), Set(4, 4, 5, 6)))
 }
---
+++ src/main/scala/monoids/semigroups/Monoid.scala
@@ -0,0 +1,43 @@
+package monoids.semigroups
+
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+}
+trait Monoid[A] extends Semigroup[A] {
+  def empty: A
+}
+object Monoid {
+  def apply[A](implicit monoid: Monoid[A]) =
+    monoid
+
+  implicit val implicitAndBoolean: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val implicitOrBoolean: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit val implicitEitherBoolean: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = (x && !y) || (!x && y)
+  }
+
+  implicit val implicitNorBoolean: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = (!x || y) && (x || !y)
+  }
+
+  implicit def implicitSetMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    override def empty: Set[A] = Set.empty
+
+    override def combine(x: Set[A], y: Set[A]): Set[A] = x union y
+  }
+}
+
